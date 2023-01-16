e3a3c38d1a290c68634758eae9b08139dad7dec1
durmusberk <durmus.berk@ozu.edu.tr>
2023-01-16 14:37:53 +0300

    handled the token and login

---
+++ Models/RequestModels/UserLoginRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Hangman.Models.RequestModels
+{
+    public class UserLoginRequestDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
