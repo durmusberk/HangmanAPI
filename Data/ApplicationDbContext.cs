6a1d7e45447acfc0f90d793e33474aa2f39ec60c
durmusberk <durmus.berk@ozu.edu.tr>
2023-01-23 14:31:32 +0300

    Added New Generic Repository And UnitofWork

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace Hangman.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext()
+        {
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
         {
@@ -14,6 +18,19 @@ namespace Hangman.Data
         public DbSet<Session> Sessions { get; set; }
         public DbSet<Word> Words { get; set; }
 
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("DefaultSQLConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+
     }
 }
---
+++ Data/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Hangman.Data
+{
+    public interface IGenericRepository<TEntity>
+    {
+        public  IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+        public Task<TEntity> GetByID(object id);
+
+        public  void InsertAsync(TEntity entity);
+
+        public void Delete(object id);
+
+        public void Delete(TEntity entityToDelete);
+
+        public void Update(TEntity entityToUpdate);
+    }
+}
